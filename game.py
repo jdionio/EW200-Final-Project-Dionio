60cea7ac2b5b6d45be630b9bb7c41ebe198341c2
jdionio <m261638@usna.edu>
2023-11-22 10:05:01 -0500

    Camera pt.2
    Changed code so camera actually works. This involved deleting background.py and its functions because they weren't needed and also moving the Camera class to the main game to prevent a circular import.

--- game.py
+++ game.py
@@ -3,10 +3,8 @@ import sys
 
 # Import necessary files
 from parameters import *
-from background import draw_background
 from player import Player, all_sprites
 from bullet import Bullet
-from camera import Camera
 
 # Initialize pygame
 pygame.init()
@@ -14,9 +12,29 @@ pygame.init()
 # Create screen
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Zombie Shooter')
-
+background = pygame.image.load("assets/ground.png").convert()
 # Make a clock
 clock = pygame.time.Clock()
+
+# Make camera that follows player around map
+class Camera(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.offset = pygame.math.Vector2()
+        self.floor_rect = background.get_rect(topleft = (0,0))
+
+    def custom_draw(self):
+        self.offset.x = player.rect.centerx - SCREEN_WIDTH // 2 # Integer division
+        self.offset.y = player.rect.centery - SCREEN_HEIGHT // 2
+
+        # Shift the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        screen.blit(background, floor_offset_pos)
+
+        # Account for offset for each  whilst moving
+        for sprite in all_sprites:
+            offset_pos = sprite.rect.topleft - self.offset
+            screen.blit(sprite.image, offset_pos)
 
 player = Player()
 camera = Camera()
@@ -30,8 +48,7 @@ all_sprites.add(player)
 
 # Main loop
 running = True
-background = screen.copy()
-draw_background(background)
+
 
 while running:
     for event in pygame.event.get():
@@ -45,7 +62,7 @@ while running:
     # Draws all sprites on screen and updates
     all_sprites.draw(screen)
     all_sprites.update()
-
+    camera.custom_draw()
     # Update display
     pygame.display.flip()
 
